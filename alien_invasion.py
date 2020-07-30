5045495c889dc869e6213a3889162e8ab7c8559b
hewittaj <hewittaj@outlook.com>
2020-07-29 19:16:19 -0500

    refactor the run_game into separate functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -14,22 +15,25 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        # Set the backgorund color.
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
+        
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-            # Redraw the screen during each pass through the loop.
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new creen."""
             self.screen.fill(self.settings.bg_color)
-
-            # Make the most recently drawn screen visible.
+            self.ship.blitme()
+            
             pygame.display.flip()
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
+        
         #Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
