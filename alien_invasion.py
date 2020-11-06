128bf3e68ad1bc2ecd194c8bb68aa46fb62f79f0
hewittaj <hewittaj@outlook.com>
2020-11-05 18:39:39 -0600

    initial commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,10 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-
+            # THE FOLLOWING SETS TO FULLSCREEN IF DESIRED
+            #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+            #self.settings.screen_width = self.screen.get_rect().width
+            #self.settings.screen_height = self.screen.get_rect().height
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
